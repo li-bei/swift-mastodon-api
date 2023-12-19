ee04622e98d538a1c71e9130fbe587b53954bb0d
Bei Li <6008854+li-bei@users.noreply.github.com>
2023-12-19 19:28:30 +0900

    Update MastodonAPI

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "MastodonAPI",
+    platforms: [.iOS(.v13), .macOS(.v10_15)],
     products: [
         .library(name: "MastodonAPI", targets: ["MastodonAPI"]),
     ],
--- Sources/MastodonAPI/MastodonAPI.swift
+++ Sources/MastodonAPI/MastodonAPI.swift
@@ -1,12 +1,46 @@
 import Foundation
+import HTTPTypes
+import HTTPTypesFoundation
 
 public struct MastodonAPI {
     private let serverURL: URL
     
     private let accessToken: String?
     
-    public init(serverURL: URL, accessToken: String? = nil) {
+    private let session: URLSession
+    
+    public init(serverURL: URL, accessToken: String? = nil, session: URLSession = .shared) {
         self.serverURL = serverURL
         self.accessToken = accessToken
+        self.session = session
+    }
+    
+    public func response<Response>(for request: Request<Response>) async throws -> Response {
+        var urlComponents = URLComponents()
+        urlComponents.path = request.path
+        guard let url = urlComponents.url(relativeTo: serverURL) else {
+            fatalError()
+        }
+        
+        var httpRequest = HTTPRequest(method: request.method, url: url, headerFields: request.headerFields)
+        if let accessToken {
+            httpRequest.headerFields[.authorization] = "Bearer \(accessToken)"
+        }
+        
+        let (data, httpResponse) = try await {
+            if let body = request.body {
+                try await session.upload(for: httpRequest, from: body)
+            } else {
+                try await session.data(for: httpRequest)
+            }
+        }()
+        
+        switch httpResponse.status.kind {
+        case .successful:
+            let response = try JSONDecoder().decode(Response.self, from: data)
+            return response
+        default:
+            fatalError()
+        }
     }
 }
