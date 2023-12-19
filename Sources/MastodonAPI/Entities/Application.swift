fc5043e7fd8ab313f1b9e58e09f18a0413b4e390
Bei Li <6008854+li-bei@users.noreply.github.com>
2023-12-19 21:01:28 +0900

    Create Entities.Application

---
+++ Sources/MastodonAPI/Entities/Application.swift
@@ -0,0 +1,11 @@
+extension Entities {
+    public struct Application: Codable, Sendable {
+        public let clientID: String
+        public let clientSecret: String
+        
+        private enum CodingKeys: String, CodingKey {
+            case clientID = "client_id"
+            case clientSecret = "client_secret"
+        }
+    }
+}
