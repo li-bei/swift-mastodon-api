77194bd5ccbefea2c3546d9d2d0e5737b4ff9af4
Bei Li <6008854+li-bei@users.noreply.github.com>
2023-12-20 00:46:18 +0900

    Create Entities.Account

---
+++ Sources/MastodonAPI/Entities/Account.swift
@@ -0,0 +1,9 @@
+extension Entities {
+    public struct Account: Codable, Sendable {
+        public let id: String
+        
+        private enum CodingKeys: String, CodingKey {
+            case id
+        }
+    }
+}
