a4945b9aafcd7ae4e00d16083c2e7026434f9205
Bei Li <6008854+li-bei@users.noreply.github.com>
2023-12-20 00:31:45 +0900

    Create Entities.Token

---
+++ Sources/MastodonAPI/Entities/Token.swift
@@ -0,0 +1,9 @@
+extension Entities {
+    public struct Token: Codable, Sendable {
+        public let accessToken: String
+        
+        private enum CodingKeys: String, CodingKey {
+            case accessToken = "access_token"
+        }
+    }
+}
