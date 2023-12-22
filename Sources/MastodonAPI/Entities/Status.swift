e1fc6340274e5481d8726c635027ec553dfe270d
Bei Li <6008854+li-bei@users.noreply.github.com>
2023-12-22 13:34:07 +0900

    Update Entities.Status

--- Sources/MastodonAPI/Entities/Status.swift
+++ Sources/MastodonAPI/Entities/Status.swift
@@ -4,21 +4,25 @@ extension Entities {
     public final class Status: Codable, Sendable {
         public let account: Account
         public let content: String
+        public let favoriteCount: Int
         public let id: String
         public let isBookmarked: Bool
         public let isFavorited: Bool
         public let isReblogged: Bool
         public let reblog: Status?
+        public let reblogCount: Int
         public let url: URL?
         
         private enum CodingKeys: String, CodingKey {
             case account
             case content
+            case favoriteCount = "favourites_count"
             case id
             case isBookmarked = "bookmarked"
             case isFavorited = "favourited"
             case isReblogged = "reblogged"
             case reblog
+            case reblogCount = "reblogs_count"
             case url
         }
     }
