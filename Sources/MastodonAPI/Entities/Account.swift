e29ca8f04101030387fa924186c64e8d79c062dc
Bei Li <6008854+li-bei@users.noreply.github.com>
2023-12-24 12:43:56 +0900

    Update Entities.Account

--- Sources/MastodonAPI/Entities/Account.swift
+++ Sources/MastodonAPI/Entities/Account.swift
@@ -3,10 +3,12 @@ import Foundation
 extension Entities {
     public struct Account: Codable, Sendable {
         public let acct: String
+        public let avatarURL: URL
         public let displayName: String
         public let emojis: [CustomEmoji]
         public let followerCount: Int
         public let followingCount: Int
+        public let headerURL: URL
         public let id: String
         public let note: String
         public let staticAvatarURL: URL
@@ -15,10 +17,12 @@ extension Entities {
         
         private enum CodingKeys: String, CodingKey {
             case acct
+            case avatarURL = "avatar"
             case displayName = "display_name"
             case emojis
             case followerCount = "followers_count"
             case followingCount = "following_count"
+            case headerURL = "header"
             case id
             case note
             case staticAvatarURL = "avatar_static"
