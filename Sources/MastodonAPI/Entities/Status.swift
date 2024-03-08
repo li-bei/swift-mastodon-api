e6ac37b494bd8c31efd5996eacc0640b95d4fa53
Bei Li <6008854+li-bei@users.noreply.github.com>
2024-03-08 22:21:13 +0900

    Create Entities.Status

---
+++ Sources/MastodonAPI/Entities/Status.swift
@@ -0,0 +1,13 @@
+extension Entities {
+    public final class Status: Codable, Sendable {
+        public let account: Account
+        public let id: String
+        public let reblog: Status?
+
+        private enum CodingKeys: String, CodingKey {
+            case account
+            case id
+            case reblog
+        }
+    }
+}
