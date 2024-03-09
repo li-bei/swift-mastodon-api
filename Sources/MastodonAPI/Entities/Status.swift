096e6338cb2625b85529ea5e077bf7d1716f6b22
Bei Li <6008854+li-bei@users.noreply.github.com>
2024-03-09 18:30:16 +0900

    Update Entities.Status

--- Sources/MastodonAPI/Entities/Status.swift
+++ Sources/MastodonAPI/Entities/Status.swift
@@ -1,13 +1,17 @@
+import Foundation
+
 extension Entities {
     public final class Status: Codable, Sendable {
         public let account: Account
         public let content: String
+        public let creationDate: Date
         public let id: String
         public let reblog: Status?
 
         private enum CodingKeys: String, CodingKey {
             case account
             case content
+            case creationDate = "created_at"
             case id
             case reblog
         }
