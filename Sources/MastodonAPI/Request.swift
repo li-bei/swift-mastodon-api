dbb18df9779c9842c4ea3d8ae17befc0a15916fe
Bei Li <6008854+li-bei@users.noreply.github.com>
2024-03-21 00:28:33 +0900

    Update Request

--- Sources/MastodonAPI/Request.swift
+++ Sources/MastodonAPI/Request.swift
@@ -11,6 +11,10 @@ public struct Request: Sendable {
 // MARK: -
 
 extension Request {
+    public static func get(_ endpoint: String) -> Request {
+        return Request(method: .get, path: endpoint)
+    }
+
     public static func post(_ endpoint: String, parameters: Encodable) throws -> Request {
         let body = try JSONEncoder().encode(parameters)
         return Request(method: .post, path: endpoint, headerFields: [.contentType: "application/json"], body: body)
