2f41c5df68e3c12dbd2a2f20712d079d46f8226e
Bei Li <6008854+li-bei@users.noreply.github.com>
2024-03-20 16:46:03 +0900

    Update MastodonAPI

--- Sources/MastodonAPI/MastodonAPI.swift
+++ Sources/MastodonAPI/MastodonAPI.swift
@@ -1,9 +1,42 @@
 import Foundation
+import HTTPTypes
+import HTTPTypesFoundation
 
-public struct MastodonAPI {
+public struct MastodonAPI: Sendable {
     private let serverURL: URL
 
-    public init(serverURL: URL) {
+    private let session: URLSession
+
+    public init(serverURL: URL, session: URLSession = .shared) {
         self.serverURL = serverURL
+        self.session = session
+    }
+
+    public func response<Response: Decodable>(_: Response.Type, for request: Request) async throws -> Response {
+        var urlComponents = URLComponents()
+        urlComponents.path = request.path
+        guard let url = urlComponents.url(relativeTo: serverURL) else {
+            throw MastodonAPIError(request: request)
+        }
+
+        let httpRequest = HTTPRequest(method: request.method, url: url, headerFields: request.headerFields)
+
+        let (data, httpResponse) = if let body = request.body {
+            try await session.upload(for: httpRequest, from: body)
+        } else {
+            try await session.data(for: httpRequest)
+        }
+
+        switch httpResponse.status.kind {
+        case .successful:
+            let response = try JSONDecoder().decode(Response.self, from: data)
+            return response
+        default:
+            if let error = try? JSONDecoder().decode(Entities.Error.self, from: data) {
+                throw error
+            } else {
+                throw MastodonAPIError(request: request)
+            }
+        }
     }
 }
