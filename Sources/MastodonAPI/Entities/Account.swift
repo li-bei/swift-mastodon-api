b80bc8ef685173fc99f9b8f390f56b6d6fcb5c38
Bei Li <6008854+li-bei@users.noreply.github.com>
2024-03-26 22:52:47 +0900

    Update Entities.Account

--- Sources/MastodonAPI/Entities/Account.swift
+++ Sources/MastodonAPI/Entities/Account.swift
@@ -1,9 +1,15 @@
 extension Entities {
     public struct Account: Codable, Sendable {
+        public let acct: String
+        public let displayName: String
         public let id: String
+        public let username: String
 
         private enum CodingKeys: String, CodingKey {
+            case acct
+            case displayName = "display_name"
             case id
+            case username
         }
     }
 }
